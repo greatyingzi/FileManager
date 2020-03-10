26fc321da47510044b7851d6c729c86aeefb9c06
greatyingzi <924068818@qq.com>
2020-03-10 14:43:07 +0800

    支持api26之后获取其他应用缓存

--- app/src/main/java/com/jiepier/filemanager/task/SysCacheScanTask.java
+++ app/src/main/java/com/jiepier/filemanager/task/SysCacheScanTask.java
@@ -1,11 +1,19 @@
 package com.jiepier.filemanager.task;
 
+import android.annotation.SuppressLint;
+import android.app.usage.StorageStats;
+import android.app.usage.StorageStatsManager;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageStatsObserver;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.RemoteException;
+
+import androidx.annotation.RequiresApi;
+
 
 import com.jiepier.filemanager.R;
 import com.jiepier.filemanager.base.App;
@@ -15,12 +23,12 @@ import com.jiepier.filemanager.task.callback.ISysScanCallBack;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+
 
 /**
  * Created by panruijie on 2017/2/15.
@@ -29,111 +37,161 @@ import rx.Observable;
 
 public class SysCacheScanTask extends AsyncTask<Void, Void, Void> {
 
-    private ISysScanCallBack mCallBack;
-    private int mScanCount;
-    private int mTotalCount;
-    private ArrayList<JunkInfo> mSysCaches;
-    private HashMap<String, String> mAppNames;
-    private long mTotalSize = 0L;
-    private boolean mIsOverTime = true;
+	private ISysScanCallBack mCallBack;
+	private int mScanCount;
+	private int mTotalCount;
+	private ArrayList<JunkInfo> mSysCaches;
+	private HashMap<String, String> mAppNames;
+	private long mTotalSize = 0L;
+	private boolean mIsOverTime = true;
 
-    public SysCacheScanTask(ISysScanCallBack callBack) {
-        this.mCallBack = callBack;
-    }
+	StorageStatsManager storageStatsManager = null;
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        Observable.timer(30 * 1000, TimeUnit.SECONDS)
-                .subscribe(aLong -> {
-                    if (mIsOverTime) {
-                        mCallBack.onOverTime();
-                    }
-                });
-    }
 
-    @Override
-    protected Void doInBackground(Void... params) {
-        mCallBack.onBegin();
+	public SysCacheScanTask(ISysScanCallBack callBack) {
+		this.mCallBack = callBack;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			storageStatsManager = (StorageStatsManager) App.getAppContext().getSystemService(Context.STORAGE_STATS_SERVICE);
+		}
+	}
 
-        if (isCancelled()) {
-            mCallBack.onCancel();
-            return null;
-        }
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		Observable.timer(30 * 1000, TimeUnit.SECONDS)
+				.subscribe(aLong -> {
+					if (mIsOverTime) {
+						mCallBack.onOverTime();
+					}
+				});
+	}
 
-        PackageManager pm = App.getAppContext().getPackageManager();
-        List<ApplicationInfo> installedPackages = pm.getInstalledApplications(PackageManager.GET_GIDS);
+	@Override
+	protected Void doInBackground(Void... params) {
+		mCallBack.onBegin();
 
-        IPackageStatsObserver.Stub observer = new PackageStatsObserver();
-        mTotalCount = installedPackages.size();
-        mSysCaches = new ArrayList<>();
-        mAppNames = new HashMap<>();
+		if (isCancelled()) {
+			mCallBack.onCancel();
+			return null;
+		}
 
-        for (int i = 0; i < mTotalCount; i++) {
-            ApplicationInfo info = installedPackages.get(i);
-            mAppNames.put(info.packageName, pm.getApplicationLabel(info).toString());
-            getPackageInfo(info.packageName, observer);
-        }
+		PackageManager pm = App.getAppContext().getPackageManager();
+		@SuppressLint("WrongConstant") List<ApplicationInfo> installedPackages = pm.getInstalledApplications(PackageManager.GET_GIDS);
 
-        mIsOverTime = false;
-        return null;
-    }
+		IPackageStatsObserver.Stub observer = new PackageStatsObserver();
+		mTotalCount = installedPackages.size();
+		mSysCaches = new ArrayList<>();
+		mAppNames = new HashMap<>();
 
-    private void getPackageInfo(String packageName, IPackageStatsObserver.Stub observer) {
+		for (int i = 0; i < mTotalCount; i++) {
+			ApplicationInfo info = installedPackages.get(i);
+			mAppNames.put(info.packageName, pm.getApplicationLabel(info).toString());
+			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+				getPackageInfo26(info.packageName, observer);
+			else {
+				getPackageInfo(info);
+			}
+		}
 
-        if (isCancelled()) {
-            return;
-        }
+		mIsOverTime = false;
+		return null;
+	}
 
-        try {
-            PackageManager pm = App.sContext.getPackageManager();
-            Method getPackageSizeInfo = pm.getClass()
-                    .getMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
+	@RequiresApi(api = Build.VERSION_CODES.O)
+	private void getPackageInfo(ApplicationInfo appInfo) {
+		try {
+			ApplicationInfo ai = App.getAppContext().getPackageManager().getApplicationInfo(appInfo.packageName, 0);
+			StorageStats storageStats = storageStatsManager.queryStatsForUid(ai.storageUuid, appInfo.uid);
+			mScanCount++;
+			if (storageStats != null) {
+				JunkInfo info = new JunkInfo();
+				info.setPackageName(appInfo.packageName)
+						.setName(appInfo.packageName)
+						.setSize(storageStats.getCacheBytes() + storageStats.getDataBytes());
 
-            getPackageSizeInfo.invoke(pm, packageName, observer);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    }
+				if (info.getSize() > 0) {
+					mSysCaches.add(info);
+					mTotalSize += info.getSize();
+				}
+				mCallBack.onProgress(info);
+			}
 
-    private class PackageStatsObserver extends IPackageStatsObserver.Stub {
+			if (mScanCount == mTotalCount) {
+				JunkInfo junkInfo = new JunkInfo();
+				junkInfo.setName(App.getAppContext().getString(R.string.system_cache))
+						.setSize(mTotalSize)
+						.setChildren(mSysCaches)
+						.setVisible(true)
+						.setChild(false)
+						.isCheck(false);
 
-        @Override
-        public void onGetStatsCompleted(PackageStats pStats, boolean succeeded) throws RemoteException {
-            mScanCount++;
-            if (succeeded && pStats != null) {
-                JunkInfo info = new JunkInfo();
-                info.setPackageName(pStats.packageName)
-                        .setName(pStats.packageName)
-                        .setSize(pStats.cacheSize + pStats.externalCacheSize);
+				/*Collections.sort(mSysCaches);
+				Collections.reverse(mSysCaches);*/
 
-                if (info.getSize() > 0) {
-                    mSysCaches.add(info);
-                    mTotalSize += info.getSize();
-                }
-                mCallBack.onProgress(info);
-            }
+				ArrayList<JunkInfo> list = new ArrayList<>();
+				list.add(junkInfo);
+				mCallBack.onFinish(list);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            if (mScanCount == mTotalCount) {
-                JunkInfo junkInfo = new JunkInfo();
-                junkInfo.setName(App.sContext.getString(R.string.system_cache))
-                        .setSize(mTotalSize)
-                        .setChildren(mSysCaches)
-                        .setVisible(true)
-                        .setChild(false)
-                        .isCheck(false);
+	//api 小于26的系统使用
+	private void getPackageInfo26(String packageName, IPackageStatsObserver.Stub observer) {
 
-                Collections.sort(mSysCaches);
-                Collections.reverse(mSysCaches);
+		if (isCancelled()) {
+			return;
+		}
+		try {
+			PackageManager pm = App.getAppContext().getPackageManager();
+			Method getPackageSizeInfo = pm.getClass()
+					.getMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
+			getPackageSizeInfo.setAccessible(true);
+			getPackageSizeInfo.invoke(pm, packageName, observer);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
 
-                ArrayList<JunkInfo> list = new ArrayList<>();
-                list.add(junkInfo);
-                mCallBack.onFinish(list);
-            }
-        }
-    }
+	private class PackageStatsObserver extends IPackageStatsObserver.Stub {
+
+		@Override
+		public void onGetStatsCompleted(PackageStats pStats, boolean succeeded) throws RemoteException {
+			mScanCount++;
+			if (succeeded && pStats != null) {
+				JunkInfo info = new JunkInfo();
+				info.setPackageName(pStats.packageName)
+						.setName(pStats.packageName)
+						.setSize(pStats.cacheSize + pStats.externalCacheSize);
+
+				if (info.getSize() > 0) {
+					mSysCaches.add(info);
+					mTotalSize += info.getSize();
+				}
+				mCallBack.onProgress(info);
+			}
+
+			if (mScanCount == mTotalCount) {
+				JunkInfo junkInfo = new JunkInfo();
+				junkInfo.setName(App.getAppContext().getString(R.string.system_cache))
+						.setSize(mTotalSize)
+						.setChildren(mSysCaches)
+						.setVisible(true)
+						.setChild(false)
+						.isCheck(false);
+
+				/*Collections.sort(mSysCaches);
+				Collections.reverse(mSysCaches);*/
+
+				ArrayList<JunkInfo> list = new ArrayList<>();
+				list.add(junkInfo);
+				mCallBack.onFinish(list);
+			}
+		}
+	}
 }
--- app/src/main/java/com/jiepier/filemanager/ui/main/MainActivity.java
+++ app/src/main/java/com/jiepier/filemanager/ui/main/MainActivity.java
@@ -1,13 +1,22 @@
 package com.jiepier.filemanager.ui.main;
 
 import android.app.DialogFragment;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Environment;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.view.ActionMode;
+
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -25,214 +34,240 @@ import com.jiepier.filemanager.util.RxBus.RxBus;
 import com.jiepier.filemanager.util.StatusBarUtil;
 
 import java.io.File;
+import java.util.List;
 
 public class MainActivity extends BaseDrawerActivity implements
-        ActionMode.Callback, FolderChooserDialog.FolderCallback, MainContact.View {
+		ActionMode.Callback, FolderChooserDialog.FolderCallback, MainContact.View {
 
-    private MainPresenter mPresenter;
-    private ActionMode mActionMode;
-    private ScannerReceiver mScannerReceiver;
-    private UnInstallReceiver mUninstallReceiver;
-    private int mChoiceCount;
+	private MainPresenter mPresenter;
+	private ActionMode mActionMode;
+	private ScannerReceiver mScannerReceiver;
+	private UnInstallReceiver mUninstallReceiver;
+	private int mChoiceCount;
 
-    @Override
-    public void initUiAndListener() {
-        super.initUiAndListener();
+	@Override
+	public void initUiAndListener() {
+		super.initUiAndListener();
 
-        //扫描sd卡的广播
-        mScannerReceiver = new ScannerReceiver();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_FINISHED);
-        intentFilter.addDataScheme("file");
-        registerReceiver(mScannerReceiver, intentFilter);
+		//扫描sd卡的广播
+		mScannerReceiver = new ScannerReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_FINISHED);
+		intentFilter.addDataScheme("file");
+		registerReceiver(mScannerReceiver, intentFilter);
 
-        //进入界面就开始扫描，进行数据更新，时间特别长，一分钟左右
-        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        intent.setData(Uri.fromFile(new File(Environment.getExternalStorageDirectory().getPath())));
-        sendBroadcast(intent);
+		//进入界面就开始扫描，进行数据更新，时间特别长，一分钟左右
+		Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		intent.setData(Uri.fromFile(new File(Environment.getExternalStorageDirectory().getPath())));
+		sendBroadcast(intent);
 
-        //监听应用卸载的广播
-        mUninstallReceiver = new UnInstallReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
-        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        filter.addAction(Intent.ACTION_PACKAGE_REPLACED);
-        filter.addDataScheme("package");
-        registerReceiver(mUninstallReceiver, filter);
+		//监听应用卸载的广播
+		mUninstallReceiver = new UnInstallReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+		filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+		filter.addAction(Intent.ACTION_PACKAGE_REPLACED);
+		filter.addDataScheme("package");
+		registerReceiver(mUninstallReceiver, filter);
 
-        mPresenter = new MainPresenter(this);
-        mPresenter.attachView(this);
-    }
+		mPresenter = new MainPresenter(this);
+		mPresenter.attachView(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+			if (!checkAppUsagePermission(this)) {
+				requestAppUsagePermission(this);
+			}
+		}
+	}
 
-        menu.findItem(R.id.paste).setVisible(!ClipBoard.isEmpty());
-        menu.findItem(R.id.cancel).setVisible(!ClipBoard.isEmpty());
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu_main, menu);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.cancel:
-                mPresenter.clickCancel();
-                return true;
-            case R.id.paste:
-                new PasteTaskExecutor(this, mSdCardFragment.getCurrentPath()).start();
-                return true;
-            case R.id.folderinfo:
-                mPresenter.clickFloderInfo(mSdCardFragment.getCurrentPath());
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		menu.findItem(R.id.paste).setVisible(!ClipBoard.isEmpty());
+		menu.findItem(R.id.cancel).setVisible(!ClipBoard.isEmpty());
+		return true;
+	}
 
-    @Override
-    public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-        this.mActionMode = actionMode;
-        return true;
-    }
+	@RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
+	public boolean checkAppUsagePermission(Context context) {
+		UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+		long currentTime = System.currentTimeMillis();
+		// try to get app usage state in last 1 min
+		List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime - 60 * 1000, currentTime);
+		return stats.size() != 0;
+	}
 
-    @Override
-    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-        menu.clear();
-        getMenuInflater().inflate(R.menu.actionmode, menu);
-        if (mChoiceCount > 1) {
-            menu.removeItem(R.id.actionrename);
-            menu.removeItem(R.id.actionopen);
-        }
+	public void requestAppUsagePermission(Context context) {
+		Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		try {
+			context.startActivity(intent);
+		} catch (ActivityNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.cancel:
+				mPresenter.clickCancel();
+				return true;
+			case R.id.paste:
+				new PasteTaskExecutor(this, mSdCardFragment.getCurrentPath()).start();
+				return true;
+			case R.id.folderinfo:
+				mPresenter.clickFloderInfo(mSdCardFragment.getCurrentPath());
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+	@Override
+	public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+		this.mActionMode = actionMode;
+		return true;
+	}
 
-        switch (menuItem.getItemId()) {
-            case R.id.actionmove:
-                mPresenter.clickMove();
-                return true;
-            case R.id.actioncopy:
-                mPresenter.clickCopy();
-                return true;
-            case R.id.actiondelete:
-                mPresenter.clickDelete();
-                return true;
-            case R.id.actionshare:
-                mPresenter.clickShare();
-                return true;
-            case R.id.actionshortcut:
-                mPresenter.clickShortCut();
-                return true;
-            case R.id.actionzip:
-                mPresenter.clickZip(mSdCardFragment.getCurrentPath());
-                return true;
-            case R.id.actionrename:
-                mPresenter.clickRename(mSdCardFragment.getCurrentPath());
-                return true;
-            case R.id.actionopen:
-                mPresenter.clickOpenMode();
-                return true;
-            case R.id.actionall:
-                mPresenter.clickSelectAll(mSdCardFragment.getCurrentPath());
-                return true;
-            default:
-                return false;
-        }
-    }
+	@Override
+	public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+		menu.clear();
+		getMenuInflater().inflate(R.menu.actionmode, menu);
+		if (mChoiceCount > 1) {
+			menu.removeItem(R.id.actionrename);
+			menu.removeItem(R.id.actionopen);
+		}
 
-    @Override
-    public void onDestroyActionMode(ActionMode actionMode) {
-        this.mActionMode = null;
-        RxBus.getDefault().post(new CleanChoiceEvent());
-    }
+		return true;
+	}
 
-    @Override
-    public void onFolderSelection(@NonNull FolderChooserDialog dialog, @NonNull File folder) {
+	@Override
+	public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
 
-        mPresenter.folderSelect(dialog, folder);
-    }
+		switch (menuItem.getItemId()) {
+			case R.id.actionmove:
+				mPresenter.clickMove();
+				return true;
+			case R.id.actioncopy:
+				mPresenter.clickCopy();
+				return true;
+			case R.id.actiondelete:
+				mPresenter.clickDelete();
+				return true;
+			case R.id.actionshare:
+				mPresenter.clickShare();
+				return true;
+			case R.id.actionshortcut:
+				mPresenter.clickShortCut();
+				return true;
+			case R.id.actionzip:
+				mPresenter.clickZip(mSdCardFragment.getCurrentPath());
+				return true;
+			case R.id.actionrename:
+				mPresenter.clickRename(mSdCardFragment.getCurrentPath());
+				return true;
+			case R.id.actionopen:
+				mPresenter.clickOpenMode();
+				return true;
+			case R.id.actionall:
+				mPresenter.clickSelectAll(mSdCardFragment.getCurrentPath());
+				return true;
+			default:
+				return false;
+		}
+	}
 
-    @Override
-    public void cretaeActionMode() {
-        if (mActionMode == null) {
-            mActionMode = startSupportActionMode(this);
-            StatusBarUtil.setColor(this, ResourceUtil.getThemeColor(this), 0);
-        }
-    }
+	@Override
+	public void onDestroyActionMode(ActionMode actionMode) {
+		this.mActionMode = null;
+		RxBus.getDefault().post(new CleanChoiceEvent());
+	}
 
-    @Override
-    public void finishActionMode() {
-        if (mActionMode != null) {
-            mActionMode.finish();
-        }
-    }
+	@Override
+	public void onFolderSelection(@NonNull FolderChooserDialog dialog, @NonNull File folder) {
 
-    @Override
-    public void refreshMenu() {
-        invalidateOptionsMenu();
-    }
+		mPresenter.folderSelect(dialog, folder);
+	}
 
-    @Override
-    public void setActionModeTitle(String title) {
-        mActionMode.setTitle(title);
-    }
+	@Override
+	public void cretaeActionMode() {
+		if (mActionMode == null) {
+			mActionMode = startSupportActionMode(this);
+			StatusBarUtil.setColor(this, ResourceUtil.getThemeColor(this), 0);
+		}
+	}
 
-    @Override
-    public void createShortCut(String[] files) {
-        for (String file : files) {
-            FileUtil.createShortcut(this, file);
-        }
-    }
+	@Override
+	public void finishActionMode() {
+		if (mActionMode != null) {
+			mActionMode.finish();
+		}
+	}
 
-    @Override
-    public void showDialog(DialogFragment dialog) {
-        dialog.show(fm_v4, TAG_DIALOG);
-    }
+	@Override
+	public void refreshMenu() {
+		invalidateOptionsMenu();
+	}
 
-    @Override
-    public void showFolderDialog(String TAG) {
-        new FolderChooserDialog.Builder(this)
-                .chooseButton(R.string.md_choose_label)  // changes label of the choose button
-                .initialPath(mSdCardFragment.getCurrentPath())  // changes initial path, defaults to external storage directory
-                .tag(TAG)
-                .goUpLabel("Up") // custom go up label, default label is "..."
-                .show();
-    }
+	@Override
+	public void setActionModeTitle(String title) {
+		mActionMode.setTitle(title);
+	}
 
-    @Override
-    public void startShareActivity(Intent intent) {
-        startActivity(Intent.createChooser(intent, getString(R.string.share)));
-    }
+	@Override
+	public void createShortCut(String[] files) {
+		for (String file : files) {
+			FileUtil.createShortcut(this, file);
+		}
+	}
 
-    @Override
-    public void setChoiceCount(int count) {
-        this.mChoiceCount = count;
-    }
+	@Override
+	public void showDialog(DialogFragment dialog) {
+		dialog.show(fm_v4, TAG_DIALOG);
+	}
 
-    @Override
-    public void startZipTask(String fileName, String[] files) {
-        final ZipTask task = new ZipTask(this, fileName);
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, files);
-    }
+	@Override
+	public void showFolderDialog(String TAG) {
+		new FolderChooserDialog.Builder(this)
+				.chooseButton(R.string.md_choose_label)  // changes label of the choose button
+				.initialPath(mSdCardFragment.getCurrentPath())  // changes initial path, defaults to external storage directory
+				.tag(TAG)
+				.goUpLabel("Up") // custom go up label, default label is "..."
+				.show();
+	}
 
-    @Override
-    public void startUnZipTask(File unZipFile, File folder) {
-        UnzipTask task = new UnzipTask(this);
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, unZipFile, folder);
-    }
+	@Override
+	public void startShareActivity(Intent intent) {
+		startActivity(Intent.createChooser(intent, getString(R.string.share)));
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mPresenter.detachView();
-        unregisterReceiver(mScannerReceiver);
-        unregisterReceiver(mUninstallReceiver);
-    }
+	@Override
+	public void setChoiceCount(int count) {
+		this.mChoiceCount = count;
+	}
+
+	@Override
+	public void startZipTask(String fileName, String[] files) {
+		final ZipTask task = new ZipTask(this, fileName);
+		task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, files);
+	}
+
+	@Override
+	public void startUnZipTask(File unZipFile, File folder) {
+		UnzipTask task = new UnzipTask(this);
+		task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, unZipFile, folder);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mPresenter.detachView();
+		unregisterReceiver(mScannerReceiver);
+		unregisterReceiver(mUninstallReceiver);
+	}
 
 }
 
